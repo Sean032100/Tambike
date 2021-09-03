a9927734daf8884850405ca83f24cf635ce2ce8e
Sean Monacillo <71977122+Sean032100@users.noreply.github.com>
2021-09-03 10:56:26 +0800

    Add files via upload

--- TAMBIKE/js/script.js
+++ TAMBIKE/js/script.js
@@ -1,4 +1,45 @@
-	$(document).ready(function() {
+	/* Navbar Scripts */
+	// jQuery to collapse the navbar on scroll
+  $(window).on('scroll load', function() {
+		if ($(".navbar").offset().top > 20) {
+			$(".fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".fixed-top").removeClass("top-nav-collapse");
+		}
+    });
+  
+    /* Counter - CountTo */
+	var a = 0;
+	$(window).scroll(function() {
+		if ($('#counter').length) { // checking if CountTo section exists in the page, if not it will not run the script and avoid errors	
+			var oTop = $('#counter').offset().top - window.innerHeight;
+			if (a == 0 && $(window).scrollTop() > oTop) {
+			$('.counter-value').each(function() {
+				var $this = $(this),
+				countTo = $this.attr('data-count');
+				$({
+				countNum: $this.text()
+				}).animate({
+					countNum: countTo
+				},
+				{
+					duration: 2000,
+					easing: 'swing',
+					step: function() {
+					$this.text(Math.floor(this.countNum));
+					},
+					complete: function() {
+					$this.text(this.countNum);
+					//alert('finished');
+					}
+				});
+			});
+			a = 1;
+			}
+		}
+    });
+  
+  $(document).ready(function() {
   var counter = 0;
 
   // Start the changing images
@@ -37,7 +78,7 @@ function loading(){
         document.getElementById("show").style.display = "block";
       }
       num++;
-    },i*65);
+    },i*20);
   };
 
 }
